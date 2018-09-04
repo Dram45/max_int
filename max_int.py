f23e183152b1837440bc9f7294660e792967931f
Dram45 <stefano10@ru.is>
2018-09-04 14:20:23 +0000

    Finished max and set up sequence algorithm

--- max_int.py
+++ max_int.py
@@ -14,11 +14,11 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
 max_int = 0 #control number
-
+#run loop aslong as number isn't negative
 while num_int >= 0:
     if num_int > max_int:
         max_int = num_int
     num_int = int(input("Input a number: "))
         
-
+#print out the largest number entered
 print("The maximum is", max_int)    # Do not change this line
---
+++ sequence.py
@@ -0,0 +1,19 @@
+Ask for how many numbers user wants
+
+Run a loop that adds together the numbers wanted
+
+set control numbers
+control number1 is 1
+control number2 is 0
+control number3 is 0
+while generated number is less than asked print numbers run the loop
+    generate number starting from 1
+    number generated is sum of the last three numbers in the sequence
+    print out sum of control numbers
+    
+    change control number2 to 1
+    change control number3 to 1
+    change control number1 to 0
+
+when loop runs again control numbers should change based on the time of the loop
+
