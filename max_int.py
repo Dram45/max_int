70e54e7cfb747c7d495eb898881e7d9bbfbc25d1
Dram45 <stefano10@ru.is>
2018-09-04 13:57:59 +0000

    Fixed syntax error in input line in while loop

--- max_int.py
+++ max_int.py
@@ -18,7 +18,7 @@ max_int = 0 #control number
 while num_int >= 0:
     if num_int > max_int:
         max_int = num_int
-    num_int = int(input("Input a number:"))
+    num_int = int(input("Input a number: "))
         
 
 print("The maximum is", max_int)    # Do not change this line
