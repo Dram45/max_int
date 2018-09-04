897bc22335cd6014a602026c23597034299324dd
Dram45 <stefano10@ru.is>
2018-09-04 13:56:43 +0000

    Implented simple code, it works

--- max_int.py
+++ max_int.py
@@ -1,14 +1,24 @@
-input a number 
+#input a number 
 
-control number is 0
+#control number is 0
 
-if input number is negative break.
+#if input number is negative break.
 
-compare input number to control number
+#compare input number to control number
 
-if input number is higher
-control number is equal to input number
-else control number stays the same
+#if input number is higher
+#control number is equal to input number
+#else control number stays the same
 
-at end of loop print out control number
+#at end of loop print out control number
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = 0 #control number
 
+while num_int >= 0:
+    if num_int > max_int:
+        max_int = num_int
+    num_int = int(input("Input a number:"))
+        
+
+print("The maximum is", max_int)    # Do not change this line
