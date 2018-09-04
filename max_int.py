ded5fd4549a1450639b63a7b139b411d93e989ad
Dram45 <stefano10@ru.is>
2018-09-04 13:41:33 +0000

    Algorith written for max_int

---
+++ max_int.py
@@ -0,0 +1,10 @@
+input a number 
+
+control number is 0
+
+if input number is negative break.
+
+compare input number to control number
+
+if input number is higher print input number
+else print control number
